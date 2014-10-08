e105105d0b8403268db859205fd9b961ea9fe75a
Shawn Hoffman (MBP) <Shawn.p.hoffman@gmail.com>
2014-10-07 21:31:33 -0400

    Updated tests.

--- test/featured.test.js
+++ test/featured.test.js
@@ -7,16 +7,26 @@ var baseURL = process.env.NODE_ENV == 'dev' ? 'http://localhost:1337' : 'http://
 describe('getting featured boardgame', function(){
 
   describe('get featured game object', function(){
-        it('should return a valid game object', function(done){
-          request.get({ url: baseURL + '/featured' },
-            function (err, res) {
-              if(err) { throw err; }
-              payload = JSON.parse(res.body);
-              payload.id.length.should.be.above(0);
-              done();
-            });
-        });
+    it('should return a valid game object', function(done){
+      request.get({ url: baseURL + '/featured' },
+      function (err, res) {
+        if(err) { throw err; }
+        payload = JSON.parse(res.body);
+        payload.id.length.should.be.above(0);
+        done();
+      });
     });
+  });
 
-
+  describe('get featured game image', function(){
+    it('should return a valid game image URL', function(done){
+      request.get({ url: baseURL + '/featured/image' },
+      function (err, res) {
+        if(err) { throw err; }
+        payload = JSON.parse(res.body);
+        payload.thumbURL.length.should.be.above(0);
+        done();
+      });
+    });
+  });
 });
