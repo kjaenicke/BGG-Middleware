5ae38812053db32bd947bb86edc25b761f03bd0d
Shawn Hoffman (MBP) <Shawn.p.hoffman@gmail.com>
2014-10-07 21:14:48 -0400

    Added featured-image route for the home screen. Upgraded Express

---
+++ routes/featured-image.js
@@ -0,0 +1,44 @@
+var ent = require('ent');
+
+module.exports = function(app, request, parseString){
+  app.get('/featured/image', function(req,res){
+      request.get({
+        url: 'http://boardgamegeek.com/xmlapi/game/' + 157354
+      }, function(error, response){
+          if(!error){
+            try{
+              //convert xml to json
+              parseString(response.body, function (err, data){
+                //reset data object because this is the only part we care about
+                data = data.boardgames.boardgame[0];
+
+                if(data){
+                  if(data.thumbnail){
+                    var thumbURL = data.thumbnail[0] || '';
+                    res.send(JSON.stringify({"thumbURL":thumbURL}));
+                    res.end();
+                  } else {
+                    res.status(500).send('No thumbnail available.');
+                    res.end();
+                  }
+                }
+                else {
+                  res.status(500).send('No game data available.');
+                  res.end();
+                }
+              });
+            }
+            catch (err){
+              res.status(500).send(err);
+              res.end();
+              throw new Error(err);
+            }
+          }
+          else{
+            res.status(500).send(e);
+            res.end();
+            throw new Error(error);
+          }
+      });
+  });
+};
