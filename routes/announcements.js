3aef9b06183a680ffd71200da9594251edc78e19
Shawn Hoffman (MBP) <Shawn.p.hoffman@gmail.com>
2014-10-27 20:36:20 -0400

    Testing some authentication

--- routes/announcements.js
+++ routes/announcements.js
@@ -1,15 +1,20 @@
 module.exports = function(app, request){
   app.get('/announcements', function(req, res){
-    request.get({ url: 'http://kjaenicke.github.io/BGG_App/announcements.json'},
-      function(error, response){
-        if(!error){
-          res.write(response.body);
-          res.end();
-        } else {
-          res.send('500');
-          res.end();
-          throw new Error(error || 'Invalid response');
-        }
-    });
+    if (process.env.NODE_ENV !== "production" || req.get('auth-token')===process.env.AUTH_TOKEN){
+      request.get({ url: 'http://kjaenicke.github.io/BGG_App/announcements.json'},
+        function(error, response){
+          if(!error){
+            res.write(response.body);
+            res.end();
+          } else {
+            res.status(500);
+            res.end();
+            throw new Error(error || 'Invalid response');
+          }
+      });
+    } else {
+      res.status(401).write('Unauthorized');
+      res.end();
+    }
   });
 };
