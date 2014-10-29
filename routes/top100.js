16c7ee65d8a9ab337c9afabdf9aee1cd30622fba
Shawn Hoffman (MBP) <Shawn.p.hoffman@gmail.com>
2014-10-28 21:35:43 -0400

    Remove gzipping

--- routes/top100.js
+++ routes/top100.js
@@ -3,42 +3,29 @@ var http = require('http');
 var zlib = require('zlib');
 
 module.exports = function(app, request){
-  app.get('/top100', function(req, response){
+  app.get('/top100', function(req, res){
     try {
-      var options = {
-          host: 'boardgamegeek.com',
-          path: '/browse/boardgame'
-      };
+      var url = 'http://boardgamegeek.com/browse/boardgame';
+      request(url, function(err, response, body){
+        if(err){ throw err; }
 
-      http.get(options, function(res) {
-        var body = '';
-        var gunzip = zlib.createGunzip();
+        var games = [];
 
-        res.pipe(gunzip);
+        $ = cheerio.load(body);
+        $('#collectionitems tr td.collection_objectname a').each(function(index, gameLink){
+          if($(gameLink).attr('href')){
+            var idUrl = $(gameLink).attr('href');
+            var id = parseInt(idUrl.substring(11, idUrl.lastIndexOf('/')), 10);
 
-        gunzip.on('data', function (data) {
-            body += data;
+            games.push({
+              id: id,
+              name: $(gameLink).text()
+            });
+          }
         });
 
-        gunzip.on('end', function() {
-          var games = [];
-
-          $ = cheerio.load(body);
-          $('#collectionitems tr td.collection_objectname a').each(function(index, gameLink){
-            if($(gameLink).attr('href')){
-              var idUrl = $(gameLink).attr('href');
-              var id = parseInt(idUrl.substring(11, idUrl.lastIndexOf('/')), 10);
-
-              games.push({
-                id: id,
-                name: $(gameLink).text()
-              });
-            }
-          });
-
-          response.write(JSON.stringify(games));
-          response.end();
-        });
+        res.write(JSON.stringify(games));
+        res.end();
       });
     }
     catch (e){
