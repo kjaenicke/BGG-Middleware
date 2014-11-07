dee83a56a34fd272b85c1becae666cf18e7911d5
Shawn Hoffman (MBP) <Shawn.p.hoffman@gmail.com>
2014-11-07 09:56:24 -0500

    Testing an issue

--- routes/mostActive.js
+++ routes/mostActive.js
@@ -20,44 +20,52 @@ module.exports = function(app, request, parseString){
           if(!error){
             try{
               var games = [];
+              console.log(response.body);
 
-              //convert xml to json
-              parseString(response.body, function (err, data) {
-                if(data.items.item){
-                  var results = data.items.item;
+              if (response.body !== undefined) {
+                //convert xml to json
+                parseString(response.body, function (err, data) {
+                  console.log(data);
+                  console.log(data.items);
+                  if(data.items && data.items.item){
+                    var results = data.items.item;
 
-                  for(var i = 0; i < results.length; i++){
-                      var game = {};
-                      game.title = results[i].name[0].$.value || undefined;
+                    for(var i = 0; i < results.length; i++){
+                        var game = {};
+                        game.title = results[i].name[0].$.value || undefined;
 
-                      // Year published
-                      if(results[i].yearpublished && results[i].yearpublished.length > 0){
-                        game.yearPublished = results[i].yearpublished[0].$.value || '';
-                      }
+                        // Year published
+                        if(results[i].yearpublished && results[i].yearpublished.length > 0){
+                          game.yearPublished = results[i].yearpublished[0].$.value || '';
+                        }
 
-                      // Game type
-                      if(results[i].$.type && results[i].$.type.length > 0){
-                        game.type = results[i].$.type || '';
-                      }
+                        // Game type
+                        if(results[i].$.type && results[i].$.type.length > 0){
+                          game.type = results[i].$.type || '';
+                        }
 
-                      // Game ID - CRUCIAL
-                      if(results[i].$.id && results[i].$.id.length > 0){
-                        game.id = results[i].$.id || '';
-                      }
+                        // Game ID - CRUCIAL
+                        if(results[i].$.id && results[i].$.id.length > 0){
+                          game.id = results[i].$.id || '';
+                        }
 
-                      games.push(game);
+                        games.push(game);
+                    }
+
+                    //limit results if parameter exists
+                    if(limit != -1){
+                      games = games.slice(0, parseInt(limit, 10));
+                    }
+
+                  } else {
+                    res.status(500).send('Invalid parseString response.');
+                    res.end();
                   }
 
-                  //limit results if parameter exists
-                  if(limit != -1){
-                    games = games.slice(0, parseInt(limit, 10));
-                  }
-
-                }
-
-                res.write(JSON.stringify(games));
-                res.end();
-              });
+                  res.write(JSON.stringify(games));
+                  res.end();
+                });
+              }
             }
             catch (e){
               res.send('500');
