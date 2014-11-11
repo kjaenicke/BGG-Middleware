c49862a773dd32a1a2c3575792604c9e2241016d
Keenan Jaenicke <kjaenick@gmail.com>
2014-11-11 11:08:07 -0500

    add tests back

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
           timeout: 10000
         },
         src: [
-        //'test/**/*.test.js',
+        'test/**/*.test.js',
         'test/**/user.*.test.js'
         ]
       }
