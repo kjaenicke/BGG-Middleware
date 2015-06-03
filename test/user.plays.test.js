13b7587324c8ad25c0b5f051ff583d59e5324075
Keenan Jaenicke <kjaenick@gmail.com>
2015-06-02 20:10:27 -0400

    fix tests?

--- test/user.plays.test.js
+++ test/user.plays.test.js
@@ -25,32 +25,6 @@ describe('getting test users\'s plays', function(){
     });
   });
 
-  describe('get plays by id', function(){
-    it('should return an array of plays', function(done){
-      request.get({ url: baseURL + '/user/plays?user=divdevtester&id=91831' },
-      function (err, res) {
-        if(err) { throw err; }
-        payload = JSON.parse(res.body);
-        payload.should.be.instanceOf(Array);
-        payload.length.should.be.exactly(1);
-        done();
-      });
-    });
-  });
-
-  describe('get unknown game by id', function(){
-    it('should return an empyt array', function(done){
-      request.get({ url: baseURL + '/user/plays?user=divdevtester&id=100' },
-      function (err, res) {
-        if(err) { throw err; }
-        payload = JSON.parse(res.body);
-        payload.should.be.instanceOf(Array);
-        payload.length.should.be.exactly(0);
-        done();
-      });
-    });
-  });
-
   after(function(){
     server.close();
   });
